184f89ba15499fe710e99960cb3aed5b730bb342
Juan Becchio <juan.becchio@4rsoluciones.com>
2022-03-14 12:28:28 -0300

    Home screen

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -2,7 +2,17 @@ import React from "react";
 import "./style.css";
 
 const Home: React.FC = () => {
-  return <div className="background">Home Home</div>;
+  return (
+    <div className="background">
+      <div className="title">
+        <h1>React / Reach</h1>
+        <h2>NFT Gallery</h2>
+      </div>
+      <div className="button">
+        <span> Go to Galery</span>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
